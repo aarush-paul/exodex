d602c60b32691a9f03619316bd6de2ca88ebec15
Aarush Paul <76874133+aarush-paul@users.noreply.github.com>
2026-09-11 15:48:56 +0000

    bug fixes

--- target/classes/com/cse/exodex/www/js/System.js
+++ target/classes/com/cse/exodex/www/js/System.js
@@ -48,9 +48,6 @@ function System(star, position, planets, lookAt) {
   var starDetail = new THREE.Mesh(DETAIL_GEOMETRY, material);
   starDetail.objectData = starData;
   starDetail.scale.x = starDetail.scale.y = starDetail.scale.z = starData.radius.value.quantity;
-  if (starData.primaryId == 1) {
-    starDetail.visible = false;
-  }
 
   object.add(starDetail);
 
